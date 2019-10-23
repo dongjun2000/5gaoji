c7aa077e22004964bd22f9172798931a1153235f
董俊 <418826102@qq.com>
2019-10-23 21:50:25 +0800

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,16 +25,19 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'  => 'required|between:2,25|regex:/^[A-Za-z0-9\_]+$/|unique:users,name,' . Auth::id(),
-            'email' => 'required|email',
-            'intro' => 'max:80',
+            'name'   => 'required|between:2,25|regex:/^[A-Za-z0-9\_]+$/|unique:users,name,' . Auth::id(),
+            'email'  => 'required|email',
+            'intro'  => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,jpg|dimensions:min_width=250,min_height=250'
         ];
     }
 
     public function messages()
     {
         return [
-            'name.regex' => '姓名只能包含大小写字母、数字和下划线，不能包含中文',
+            'name.regex'        => '姓名只能包含大小写字母、数字和下划线，不能包含中文',
+            'avatar.mimes'      => '头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 250px 以上',
         ];
     }
 
