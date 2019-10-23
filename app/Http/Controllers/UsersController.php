f08d13b5b4129a8e4ab322b9f43f71ef795aab4a
董俊 <418826102@qq.com>
2019-10-23 21:11:30 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -18,10 +19,19 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
-//        dd($user);
-        $user->update($request->all());
+        $data = $request->all();
+
+        // 上传图片逻辑
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user)->with('success', '个人资料更新成功！');
     }
 }
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,7 +9,7 @@
                 <h4><i class="fa fa-user-edit"></i>编辑个人资料</h4>
             </div>
             <div class="card-body">
-                <form action="{{ route('users.update', $user) }}" method="post">
+                <form action="{{ route('users.update', $user) }}" method="post" enctype="multipart/form-data">
                     {{ method_field('PUT') }}
                     {{ csrf_field() }}
 
@@ -17,17 +17,32 @@
 
                     <div class="form-group">
                         <label for="name">昵称：</label>
-                        <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $user->name) }}">
-                        <small class="form-text text-muted"><i class="fa fa-info-circle"></i>姓名只能包含大小写字母、数字和下划线，不能包含中文</small>
+                        <input type="text" class="form-control" id="name" name="name"
+                               value="{{ old('name', $user->name) }}">
+                        <small class="form-text text-muted"><i class="fa fa-info-circle"></i>姓名只能包含大小写字母、数字和下划线，不能包含中文
+                        </small>
                     </div>
                     <div class="form-group">
                         <label for="email">邮箱：</label>
-                        <input type="text" class="form-control" id="email" name="email" value="{{ old('email', $user->email) }}">
+                        <input type="text" class="form-control" id="email" name="email"
+                               value="{{ old('email', $user->email) }}">
                     </div>
                     <div class="form-group">
                         <label for="intro">个人简介：</label>
-                        <textarea class="form-control" id="intro" name="intro" rows="3">{{ old('intro', $user->intro) }}</textarea>
+                        <textarea class="form-control" id="intro" name="intro"
+                                  rows="3">{{ old('intro', $user->intro) }}</textarea>
                     </div>
+
+                    <div class="form-group mb-4">
+                        <label for="avatar" class="avatar-label">用户头像</label>
+                        <input type="file" id="avatar" name="avatar" class="form-control-file">
+
+                        @if($user->avatar)
+                            <br>
+                            <img class="img-thumbnail" src="{{ $user->avatar }}" width="200">
+                        @endif
+                    </div>
+
                     <button class="btn btn-primary">保存</button>
                 </form>
             </div>
